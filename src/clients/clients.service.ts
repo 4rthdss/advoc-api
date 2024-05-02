9c689b07b2fd5ef28995e276e4ea3b974592b1c1
Arthur Silva <fortherain4@gmail.com>
2024-05-02 10:28:58 -0300

    created GET and POST clients

---
+++ src/clients/clients.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateClientDto } from './dto/create-client.dto';
+import { UpdateClientDto } from './dto/update-client.dto';
+import { Client } from './entities/client.entity';
+
+@Injectable()
+export class ClientsService {
+  private readonly clients: Client[] = [];
+  private id = 1;
+
+  create(createClientDto: CreateClientDto) {
+    const newClient = {
+      id: this.id,
+      name: createClientDto.name,
+      email: createClientDto.email,
+      cpf: createClientDto.cpf,
+    };
+    this.id = this.id + 1;
+    this.clients.push(newClient);
+    return newClient;
+  }
+
+  findAll() {
+    return this.clients;
+  }
+
+  findOne(id: number) {
+    const client = this.clients.find((client) => client.id == id);
+    if (!client) {
+      throw new NotFoundException('Client not found');
+    }
+    return client;
+  }
+
+  update(id: number, updateClientDto: UpdateClientDto) {
+    return `This action updates a #${id} client`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} client`;
+  }
+}
